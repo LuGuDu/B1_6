673582d0fd415ab59eb0d4b84da360a98797a11a
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2020-10-01 14:50:20 +0200

    Interfaz y dibujo del json
    Funcionalidad de dibujar un laberinto a partir de un json añadida

--- practica/Principal.java
+++ practica/Principal.java
@@ -1,11 +1,25 @@
 package practica;
 
+import java.awt.Image;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JFrame;
+
 public class Principal {
 	public static void main(String[] args) {
-
+		
+		JFrame frame = new JFrame("Grupo B1-6 -- Practica de Laboratorio");
 		Labyrinth lab = null;
-		lab = practica.ReadJson.ReadJsons();
-		System.out.println(lab);
+
+		lab = practica.ReadJson.ReadJsons();		
+		System.out.println(lab);		
+		//practica.Interface.drawLab(lab);
 		practica.WriteJson.writeJson(lab);
+
+		frame.getContentPane().add(new Interface(lab));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1280, 720);
+		frame.setVisible(true);
 	}
 }
