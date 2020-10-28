532881582fb61c41b1e121fa3a1773e85632387f
David Gutierrez <72127510+david8gutti@users.noreply.github.com>
2020-10-28 14:13:29 +0100

    Comentarios añadidos metodo writeJsonProblem

--- SistemasInteligentes/src/practica/WriteJson.java
+++ SistemasInteligentes/src/practica/WriteJson.java
@@ -7,12 +7,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 /**
  * Esta clase 
- * @author David Gonz·lez Berm˙dez, Lucas GutiÈrrez Dur·n, David GutiÈrrez Mariblanca
+ * @author David Gonz√°lez Berm√∫dez, Lucas Guti√©rrez Dur√°n, David Guti√©rrez Mariblanca
  * Fecha: 16/10/2020
  */
 public class WriteJson {
 	/**
-	 * MÈtodo que escribe un archivo .json a partir de un objeto Labyrinth y un nombre de
+	 * M√©todo que escribe un archivo .json a partir de un objeto Labyrinth y un nombre de
 	 * archivo.
 	 * @param lab
 	 * @param name
@@ -31,6 +31,13 @@ public class WriteJson {
 		}
 
 	}
+	
+	/**
+	 * M√©todo que escribe un archivo .json a partir de un objeto Labyrinth y un nombre de
+	 * archivo para poder general el problema.
+	 * @param prob
+	 * @param name
+	 */
 	
 	public static void writeJsonProblem(Problem prob, String name) {
 
