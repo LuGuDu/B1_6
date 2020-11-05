0468313bf678358a3e9bca6b1aae08646b217385
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2020-11-05 19:03:05 +0100

    Aplicado 'format'

--- SistemasInteligentes/src/practica/busqueda/SearchAlgorithm.java
+++ SistemasInteligentes/src/practica/busqueda/SearchAlgorithm.java
@@ -9,7 +9,7 @@ public class SearchAlgorithm {
 		ArrayList<Node> visited = new ArrayList<Node>();
 		Border border = new Border();
 		boolean isSolution = false;
-		
+
 		// Nodo inicial
 		Node node = new Node();
 		node.setFather(null);
@@ -19,104 +19,101 @@ public class SearchAlgorithm {
 		node.setAction(null);
 		node.setHeuristic(heuristic(problem, node.getIdState()));
 		node.setValue(calculate(strategy, node));
-		
+
 		border.push(node);
 		while (!border.getFrontier().isEmpty() && !isSolution) {
 			node = border.pop();
 			if (problem.getObjective().equals(node.getIdState())) {
 				isSolution = true;
-			}
-			else if (!visited.contains(node) && node.getDepth() < depth) {
+			} else if (!visited.contains(node) && node.getDepth() < depth) {
 				visited.add(node);
 				ArrayList<Node> expandNodes = expandNode(problem, node, strategy);
 				while (!expandNodes.isEmpty()) {
-					border.push(expandNodes.remove(expandNodes.size()-1));
+					border.push(expandNodes.remove(expandNodes.size() - 1));
 				}
 			}
 		}
 		if (isSolution) {
 
 			return road(node);
-		}
-		else {
+		} else {
 			return null;
 		}
 	}
-	
+
 	public static ArrayList<Node> expandNode(Problem problem, Node node, int strategy) {
 		ArrayList<Node> expandList = new ArrayList<Node>();
 		ArrayList<Sucesor> sucesors = problem.getSucesors(node.getIdState());
 		while (!sucesors.isEmpty()) {
 			Sucesor suc = new Sucesor();
-			suc = sucesors.remove(sucesors.size()-1);
+			suc = sucesors.remove(sucesors.size() - 1);
 			Node childNode = new Node();
 			if (node.getFather() != null) {
 				if (!node.getFather().getIdState().equals(suc.getIdState())) {
 					childNode.setIdState(suc.getIdState());
 					childNode.setFather(node);
 					childNode.setAction(suc.getMov());
-					childNode.setDepth(node.getDepth()+1);
-					childNode.setCost(node.getCost()+suc.getCost());
+					childNode.setDepth(node.getDepth() + 1);
+					childNode.setCost(node.getCost() + suc.getCost());
 					childNode.setHeuristic(heuristic(problem, node.getIdState()));
 					childNode.setValue(calculate(strategy, node));
 					expandList.add(childNode);
 				}
-			}
-			else {
+			} else {
 				childNode.setIdState(suc.getIdState());
 				childNode.setFather(node);
 				childNode.setAction(suc.getMov());
-				childNode.setDepth(node.getDepth()+1);
-				childNode.setCost(node.getCost()+suc.getCost());
+				childNode.setDepth(node.getDepth() + 1);
+				childNode.setCost(node.getCost() + suc.getCost());
 				childNode.setHeuristic(heuristic(problem, node.getIdState()));
 				childNode.setValue(calculate(strategy, node));
 				expandList.add(childNode);
 			}
 		}
-		
+
 		return expandList;
 	}
-	
-	
+
 	public static double heuristic(Problem problem, String idState) {
 		int finalRow = Functions.getRow(problem.getObjective());
 		int finalCol = Functions.getCol(problem.getObjective());
 		int row = Functions.getRow(idState);
 		int col = Functions.getCol(idState);
-		double heuristic = Math.abs((row-finalRow)) + Math.abs((col-finalCol));
+		double heuristic = Math.abs((row - finalRow)) + Math.abs((col - finalCol));
+
 		return heuristic;
 	}
-	
+
 	public static double calculate(int strategy, Node node) {
 		double value = 0;
 		switch (strategy) {
 		case 1:
-			value = node.getCost() + node.getHeuristic(); //Estrategia A*
+			value = node.getCost() + node.getHeuristic(); // Estrategia A*
 			break;
 		case 2:
-			value = node.getCost(); //Estrategia costo uniforme
+			value = node.getCost(); // Estrategia costo uniforme
 			break;
 		case 3:
-			value = node.getDepth();//Estrategia en anchura
+			value = node.getDepth();// Estrategia en anchura
 			break;
 		case 4:
-			value = 1/(node.getDepth() + 1);//Estrategia en profundidad
+			value = 1 / (node.getDepth() + 1);// Estrategia en profundidad
 			break;
 		case 5:
-			value = node.getHeuristic();//Estrategia voraz
+			value = node.getHeuristic();// Estrategia voraz
 			break;
 		}
-		
+
 		return value;
 	}
-	
-	public static ArrayList<Node> road (Node node) {
+
+	public static ArrayList<Node> road(Node node) {
 		ArrayList<Node> solution = new ArrayList<Node>();
 		while (node.getFather() != null) {
 			solution.add(node);
 			node = node.getFather();
 		}
-		
+
 		return solution;
 	}
 
