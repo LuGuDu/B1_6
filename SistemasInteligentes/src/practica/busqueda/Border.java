f0681d289b5e9bd1d59f1158cf64a313323a5c1f
DavidGB18 <71131821+DavidGB18@users.noreply.github.com>
2020-11-09 23:32:18 +0100

    Ordenado sin pryority queue

--- SistemasInteligentes/src/practica/busqueda/Border.java
+++ SistemasInteligentes/src/practica/busqueda/Border.java
@@ -1,7 +1,8 @@
 package practica.busqueda;
 
 import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.*;
 
 /**
  * Esta clase representa la frontier de nuestro arbol, se 
@@ -13,18 +14,19 @@ import java.util.PriorityQueue;
  */
 
 public class Border {
-	private Comparator<Node> comparator = new Order();
-	private PriorityQueue<Node> frontier = new PriorityQueue<Node>(50, comparator);
+	LinkedList<Node> frontier = new LinkedList<Node>();
+	
 	
 	public Border () {
 		
 	}
-	public Border (PriorityQueue<Node> frontier) {
+	public Border (LinkedList<Node> frontier) {
 		this.frontier = frontier;
 	}
 	
 	public void push(Node node) {
-		frontier.add(node);
+		frontier.offer(node);
+		Collections.sort(frontier,Comparator.comparing(Node::getValue).thenComparing(Node::getRow).thenComparing(Node::getCol));
 	}
 
 	public Node pop() {
@@ -36,41 +38,10 @@ public class Border {
 	}
 
 
-	public PriorityQueue<Node> getFrontier() {
+	public LinkedList<Node> getFrontier() {
 		return frontier;
 	}
-	public void setFrontier(PriorityQueue<Node> frontier) {
+	public void setFrontier(LinkedList<Node> frontier) {
 		this.frontier = frontier;
-	}
-
-
-	public class Order implements Comparator<Node> {
-		public int compare(Node x, Node y) {
-			if (x.getValue() > y.getValue()) {
-				return -1;
-			}
-			else if (x.getValue() < y.getValue()) {
-				return 1;
-			}
-			else {
-				if (x.getRow() > y.getRow()) {
-					return -1;
-				}
-				else if (x.getRow() < y.getRow()) {
-					return 1;
-				}
-				else {
-					if (x.getCol() > y.getCol()) {
-						return -1;
-					}
-					else if (x.getCol() < y.getCol()) {
-						return 1;
-					}
-					else {
-						return 0;
-					}
-				}
-			}
-		}
 	}
 }
