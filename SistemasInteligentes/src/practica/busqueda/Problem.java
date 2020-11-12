6386cc27d7c293960e56f38dd1e09a8327eb61e2
DavidGB18 <71131821+DavidGB18@users.noreply.github.com>
2020-11-12 12:41:27 +0100

    Cambio en costes de sucesores

--- SistemasInteligentes/src/practica/busqueda/Problem.java
+++ SistemasInteligentes/src/practica/busqueda/Problem.java
@@ -73,16 +73,16 @@ public class Problem {
 		boolean[] neighbours = Arrays.copyOf(cellCheck.getNeighbors(), 4);
 
 		if (neighbours[3]) {
-			sucesors.add(new Sucesor("O", "(" + rowO + ", " + colO + ")", cells.get("(" + row + ", " + col + ")").getValue()));
+			sucesors.add(new Sucesor("O", "(" + rowO + ", " + colO + ")", cells.get("(" + rowO + ", " + colO + ")").getValue()));
 		}
 		if (neighbours[2]) {
-			sucesors.add(new Sucesor("S", "(" + rowS + ", " + colS + ")", cells.get("(" + row + ", " + col + ")").getValue()));
+			sucesors.add(new Sucesor("S", "(" + rowS + ", " + colS + ")", cells.get("(" + rowS + ", " + colS + ")").getValue()));
 		}
 		if (neighbours[1]) {
-			sucesors.add(new Sucesor("E", "(" + rowE + ", " + colE + ")", cells.get("(" + row + ", " + col + ")").getValue()));
+			sucesors.add(new Sucesor("E", "(" + rowE + ", " + colE + ")", cells.get("(" + rowE + ", " + colE + ")").getValue()));
 		}
 		if (neighbours[0]) {
-			sucesors.add(new Sucesor("N", "(" + rowN + ", " + colN + ")", cells.get("(" + row + ", " + col + ")").getValue()));
+			sucesors.add(new Sucesor("N", "(" + rowN + ", " + colN + ")", cells.get("(" + rowN + ", " + colN + ")").getValue()));
 		} 
 		
 		return sucesors;
