10dbdb1485024f028480aab437d256f38a6eebdb
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2020-11-16 19:08:55 +0100

    solucionado impresion de double en txt en estrategia profundidad

--- SistemasInteligentes/src/practica/busqueda/PrintSolution.java
+++ SistemasInteligentes/src/practica/busqueda/PrintSolution.java
@@ -9,14 +9,14 @@ import java.util.ArrayList;
 import java.util.Deque;
 
 /**
- * Esta clase se encarga de guardar en un archivo nuestra solucion con la estrategia correspondiente
- * Tiene como atributo la solucion y la estrategia
+ * Esta clase se encarga de guardar en un archivo nuestra solucion con la
+ * estrategia correspondiente Tiene como atributo la solucion y la estrategia
  * 
- * @author David González Bermúdez, Lucas Gutiérrez Durán, David Gutiérrez Mariblanca
- * Fecha: 14/11/2020
+ * @author David González Bermúdez, Lucas Gutiérrez Durán, David Gutiérrez
+ *         Mariblanca Fecha: 14/11/2020
  */
 public class PrintSolution {
-	
+
 	/**
 	 * Nos permite guardar nuestra solucion segun la estrategia correspondiente
 	 * 
@@ -25,14 +25,15 @@ public class PrintSolution {
 	 * 
 	 */
 	public static void printSolution(ArrayList<Node> solution, int strategy) {
-		
-		String path = System.getProperty("user.home")+"/Desktop";	
+
+		String path = System.getProperty("user.home") + "/Desktop";
+		double value;
 		Deque<Node> nodesInverse = new ArrayDeque<Node>();
-		for (Node n: solution) {
+		for (Node n : solution) {
 			nodesInverse.push(n);
 		}
 		String nameStrategy = null;
-		
+
 		switch (strategy) {
 		case 1:
 			nameStrategy = "Anchura";
@@ -50,29 +51,40 @@ public class PrintSolution {
 			nameStrategy = "A";
 			break;
 		}
-		
-		path=path+"/Solucion_"+nameStrategy+".txt";
+
+		path = path + "/Solucion_" + nameStrategy + ".txt";
 		try {
 			BufferedWriter out = new BufferedWriter(new FileWriter(new File(path)));
 			out.flush();
 			out.write("[id][cost,state,father_id,action,depth,h,value]");
 			out.newLine();
-			
-			while(!nodesInverse.isEmpty()) {
-				Node n=nodesInverse.pop();
-				out.write("["+n.getId()+"]");
-				out.write("["+(int)n.getCost()+","+n.getIdState()+","+n.getFatherId()+","+n.getAction()+","+n.getDepth()+","+ //FALTA IMPRIMIR PADRE
-				(int)n.getHeuristic()+","+(int)n.getValue()+"]");
-				out.newLine();		
+
+			while (!nodesInverse.isEmpty()) {
+				Node n = nodesInverse.pop();
+				out.write("[" + n.getId() + "]");
+
+				switch (strategy) {
+				case 2:
+					out.write("[" + (int) n.getCost() + "," + n.getIdState() + "," + n.getFatherId() + ","
+							+ n.getAction() + "," + n.getDepth() + "," + // FALTA IMPRIMIR PADRE
+							(int) n.getHeuristic() + "," + n.getValue() + "]");
+					out.newLine();
+					break;
+
+				default:
+					out.write("[" + (int) n.getCost() + "," + n.getIdState() + "," + n.getFatherId() + ","
+							+ n.getAction() + "," + n.getDepth() + "," + // FALTA IMPRIMIR PADRE
+							(int) n.getHeuristic() + "," + (int) n.getValue() + "]");
+					out.newLine();
+				}
 			}
 			out.close();
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		
 	}
-	
+
 }
